9a3fe4f6b7a34e71f1e2cf67d251fe65e992f2d3
qc-eska <qc@telcon.pl>
2026-04-16 16:08:16 +0200

    Update channel_branding.py

--- telegram/channel_branding.py
+++ telegram/channel_branding.py
@@ -6,19 +6,34 @@ BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 CHAT_ID = os.getenv("CHAT_ID")
 
 
-def set_channel_photo(photo):
-    print("[PHOTO]", photo)
-        return
+def set_channel_photo(image_path):
+    print("[PHOTO]", image_path)
 
     url = f"{BASE_URL}/setChatPhoto"
 
     with open(image_path, "rb") as photo:
-        r = requests.post(url, data={"chat_id": CHAT_ID}, files={"photo": photo})
+        r = requests.post(
+            url,
+            data={"chat_id": CHAT_ID},
+            files={"photo": photo}
+        )
 
-    print("[BRANDING]", r.json())
+    print("[BRANDING PHOTO]", r.json())
     return r.json()
 
 
 def set_channel_title(title):
     print("[TITLE]", title)
+
+    url = f"{BASE_URL}/setChatTitle"
+
+    r = requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "title": title
+        }
+    )
+
+    print("[BRANDING TITLE]", r.json())
     return r.json()
