f5b2dc780a064baf1579b2273b82801876951803
qc-eska <qc@telcon.pl>
2026-04-16 13:05:25 +0200

    Update price_fetcher.py

--- core/price_fetcher.py
+++ core/price_fetcher.py
@@ -1,40 +1,31 @@
 import requests
-
-URL = "https://stooq.pl/q/l/?s=jsw_pl"
-
+import re
 
 CACHE = {
     "price": None
 }
 
 
-def parse(text):
-    lines = text.strip().split("\n")
+URL = "https://finance.yahoo.com/quote/JSW.WA"
 
-    if len(lines) < 2:
-        return None
 
-    row = lines[1].split(",")
-
-    # ostatnia kolumna = Close (najpewniejsze)
-    try:
-        price = float(row[4])
-        return price
-    except:
-        return None
+def get_price_from_html(html):
+    match = re.search(r'"regularMarketPrice":\{"raw":([0-9.]+)', html)
+    if match:
+        return float(match.group(1))
+    return None
 
 
 def get_jsw_price():
     try:
-        r = requests.get(URL, timeout=10)
+        r = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
 
         print("STATUS:", r.status_code)
-        print("RAW:", r.text)
 
         if r.status_code != 200:
             return None
 
-        price = parse(r.text)
+        price = get_price_from_html(r.text)
 
         if price:
             CACHE["price"] = price
